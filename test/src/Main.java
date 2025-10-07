22f18cf6c93819747f1a1b95d6852a45250ccab7
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-10-07 13:23:00 -0400

    mmm

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+ public class Main {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 1;
+
+        if ( x < y) {
+            x += 2;
+            y -= x;
         }
+        else {
+            x--;
+            y++;
+        }
+
+        if (x % 2 == 0) {
+            x *= y;
+        } else if (y % 2 == 0) {
+            y *= 2;
+        }
+
+        if (x > y) {
+            x /= 2;
+        }
+
+        System.out.println("x= "+ x);
+        System.out.println("y= "+ y);
     }
 }
