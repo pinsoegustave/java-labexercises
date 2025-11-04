eb10fe6ec8b18b72bf6a2cb971ff4cf27f044c1b
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-03 22:52:39 -0500

    array practices

--- test/src/testPush.java
+++ test/src/testPush.java
@@ -1,5 +1,17 @@
 public class testPush {
     public static void main(String[] args) {
-        System.out.println("Git test");
+
+        String[] names = new String[5];
+
+        names[0] = "Gustave";
+        names[1] = "Pinsoe";
+        names[2] = "Jamie";
+        names[3] = "Ntwali";
+        names[4] = "Mudasubira";
+
+        for (int x = 0; x <= names.length-1; x++) {
+            System.out.println(names[x]);
+        }
+
     }
 }
