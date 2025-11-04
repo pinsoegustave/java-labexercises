cbb4d07617a4d73d5a4083cfb0773603463e98f8
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-04 12:19:22 -0500

    sum of minimum - incomplete

---
+++ java-codewars/src/SumOfMinimum.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class SumOfMinimum {
+    public static int sumOfMinimums(int[][] arr){
+        Arrays.sort(arr);
+
+        System.out.println(arr);
+
+        return arr;
+    }
+}
