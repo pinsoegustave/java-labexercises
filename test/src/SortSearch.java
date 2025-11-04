7d267767767e2316d6a6e3d736d59f3dcd6e4d3b
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-03 23:11:40 -0500

    sorting with built-in function

---
+++ test/src/SortSearch.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class SortSearch {
+    public static void main(String[] args) {
+
+        int[] myNumbers = new int[10];
+
+        myNumbers[0] = 34;
+        myNumbers[1] = 23;
+        myNumbers[2] = 45;
+        myNumbers[3] = 90;
+        myNumbers[4] = 93;
+        myNumbers[5] = 45;
+        myNumbers[6] = 28;
+        myNumbers[7] = 55;
+        myNumbers[8] = 39;
+        myNumbers[9] = 79;
+
+//        display without for loop
+//        System.out.println(myNumbers);   This prints the memory address of the whole array.
+
+        Arrays.sort(myNumbers);
+
+        for (int x = 0; x <= myNumbers.length-1; x++) {
+            System.out.println(myNumbers[x]);
+        }
+    }
+}
