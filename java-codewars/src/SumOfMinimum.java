c733898c7baf61f77b68f3a59f8db659f4676606
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-06 14:39:51 -0500

    sum of minimum - tried 02

--- java-codewars/src/SumOfMinimum.java
+++ java-codewars/src/SumOfMinimum.java
@@ -1,11 +1,17 @@
 import java.util.Arrays;
 
 public class SumOfMinimum {
-    public static int sumOfMinimums(int[][] arr){
+    public static void main(String[] args) {
+        int[][] arr = {{90, 43, 23, 5}, {12, 4, 67, 48}, {8, 21, 87, 32 }};
+
         Arrays.sort(arr);
 
-        System.out.println(arr);
+        for (int x = 0; x < arr.length; x++) {
 
-        return arr;
+            for (int y = 0; y < arr.length; y++) {
+                System.out.println(arr[x][y]);
+            }
+        }
+
     }
 }
