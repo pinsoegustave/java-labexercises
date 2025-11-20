442a65ffdd051acad9fb4f477d1106ce8baab8d1
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-20 14:47:34 -0500

    java-learning: Magic Square

---
+++ test/src/MagicSquare.java
@@ -0,0 +1,183 @@
+
+public class MagicSquare {
+
+    private int size, sum;
+
+//    Constructor
+    public MagicSquare() {
+        size = 3;
+        sum = 15;
+    }
+
+//    This causes the magic square to prepare to solve
+    public void start() {
+//        Initialize the board
+        int[][] board = new int[size][size];
+
+        for (int r = 0; r < size; r++) {
+            for (int c = 0; c < size; c++) {
+                board[r][c] = 0;
+            }
+        }
+//        Test code
+        board[0][0] = 9;
+        board[0][2] = 1;
+        boolean result = canNumberBePlaced(board, 0, 1, 5);
+        System.out.println(result);
+        displayBoard(board);
+    }
+//    end start method
+
+//    Display the board
+    public void displayBoard(int[][] argBoard) {
+        for (int r = 0; r < size; r++) {
+            for (int c=0; c < size; c++) {
+                System.out.println(argBoard[r][c]);
+                System.out.println("\t");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public boolean canNumberBePlaced(int[][] argBoard, int argRow, int argCol, int argNumber) {
+//        See if the number has already been used
+        for (int r = 0; r < size; r++) {
+            for (int c= 0; c< size; c++) {
+                if (argBoard[r][c] == argNumber) {
+                    return false;
+                }
+            }
+//            Get the current total and number of boxes filled in the row
+            int currentTotal = 0;
+            int currentNumValues = 0;
+
+            for (int c = 0; c < size; c++) {
+                if (argBoard[argRow][c] > 0) {
+                    currentTotal += argBoard[argRow][c];
+                    currentNumValues++;
+                }
+            }
+            if (currentNumValues == size - 1){
+//                The number will complete the row
+//                The total with this number must = sum
+                if (currentTotal + argNumber != sum) {
+                    return false;
+                }
+            } else {
+//                The number will not complete the row
+//                The total with this number must be < sum
+                if (currentTotal + argNumber >= sum) {
+                    return false;
+                }
+            }
+
+
+        }
+//        **************************************
+//            Get the current total and number of boxes filled in the column
+        int currentTotal = 0;
+        int currentNumValues = 0;
+
+        for (int c = 0; c < size; c++) {
+            if (argBoard[argRow][c] > 0) {
+                currentTotal += argBoard[argRow][c];
+                currentNumValues++;
+            }
+        }
+        if (currentNumValues == size - 1){
+//                The number will complete the column
+//                The total with this number must = sum
+            if (currentTotal + argNumber != sum) {
+                return false;
+            }
+        } else {
+//                The number will not complete the row
+//                The total with this number must be < sum
+            if (currentTotal + argNumber >= sum) {
+                return false;
+            }
+        }
+//        ****************************************
+
+//          Get the current total and number of boxes filled in the diagonal
+        currentTotal = 0;
+        currentNumValues = 0;
+
+        for (int x = 0; x < size; x++) {
+            if (argBoard[x][x] > 0) {
+                currentTotal += argBoard[x][x];
+                currentNumValues++;
+            }
+        }
+        if (currentNumValues == size - 1){
+//                The number will complete the diagonal
+//                The total with this number must = sum
+            if (currentTotal + argNumber != sum) {
+                return false;
+            }
+        } else {
+//                The number will not complete the diagonal
+//                The total with this number must be < sum
+            if (currentTotal + argNumber >= sum) {
+                return false;
+            }
+        }
+//        *******************************
+//            Get the current total and number of boxes filled in the diagonal
+        currentTotal = 0;
+        currentNumValues = 0;
+
+        for (int x = 0; x < size; x++) {
+            if (argBoard[x][x] > 0) {
+                currentTotal += argBoard[x][x];
+                currentNumValues++;
+            }
+        }
+        if (currentNumValues == size - 1){
+//                The number will complete the row
+//                The total with this number must = sum
+            if (currentTotal + argNumber != sum) {
+                return false;
+            }
+        } else {
+//                The number will not complete the row
+//                The total with this number must be < sum
+            if (currentTotal + argNumber >= sum) {
+                return false;
+            }
+        }
+
+
+//        If we get here, all the above checks were okay.
+        return true;
+    }
+//    end of canNumberBePlaced
+
+//    Return true if solved, false otherwise
+    public boolean isSolved(int[][] argBoard) {
+        for (int r = 0; r <size; r++) {
+            for (int c = 0; c < size; c++) {
+                if (argBoard[r][c] == 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+//    end is solved
+
+    public String findFirstOpenSquare(int[][] argBoard) {
+        for (int r = 0; r < size; r++) {
+            for (int c = 0; c < size; c++) {
+                if (argBoard[r][c] == 0) {
+                    return ""+r+c;
+                }
+            }
+        }
+
+//        This should never happen
+        return "-1";
+    }
+
+}
---
+++ test/src/MagicSquareDriver.java
@@ -0,0 +1,7 @@
+public class MagicSquareDriver {
+    public static void main(String[] args) {
+
+        MagicSquare m = new MagicSquare();
+        m.start();
+    }
+}
