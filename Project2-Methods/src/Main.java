31aca73f912b282218b6eada7e7cce60b50fc001
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-12-08 15:11:59 -0500

    Project #2: Test cases

--- Project2-Methods/src/Main.java
+++ Project2-Methods/src/Main.java
@@ -6,66 +6,66 @@ public class Main {
 //        Test Cases
         PinsoeGustaveMethods methods = new PinsoeGustaveMethods();
 //
-//        System.out.println(methods.canBuildFence(2, 1, 8));
-//        System.out.println(methods.canBuildFence(2, 1, 11));
-//        System.out.println(methods.canBuildFence(4, 2, 21));
-//        System.out.println(methods.canBuildFence(4, 2, 12));
-//        System.out.println(methods.canBuildFence(10, 3, 34));
+        System.out.println(methods.canBuildFence(2, 1, 8));
+        System.out.println(methods.canBuildFence(2, 1, 11));
+        System.out.println(methods.canBuildFence(4, 2, 21));
+        System.out.println(methods.canBuildFence(4, 2, 12));
+        System.out.println(methods.canBuildFence(10, 3, 34));
 
-//        System.out.println(methods.middleOfAB("magic"));
-//        System.out.println(methods.middleOfAB("albqny"));
-//        System.out.println(methods.middleOfAB("adverb"));
-//        System.out.println(methods.middleOfAB("bean"));
+        System.out.println(methods.middleOfAB("magic"));
+        System.out.println(methods.middleOfAB("albqny"));
+        System.out.println(methods.middleOfAB("adverb"));
+        System.out.println(methods.middleOfAB("bean"));
 
 //        Player players = new Player("Abc", 0.285);
 //        System.out.println(methods.bonusQualified(0.380, players));
 
-//        System.out.println(methods.evilE("HELLO"));
-//        System.out.println(methods.evilE("Exit"));
-//        System.out.println(methods.evilE("FLEX"));
-//        System.out.println(methods.evilE("bean"));
-//        System.out.println(methods.evilE("aaxEXbb"));
-//        System.out.println(methods.evilE("EEE"));
-//        System.out.println(methods.evilE("EXE"));
+        System.out.println(methods.evilE("HELLO"));
+        System.out.println(methods.evilE("Exit"));
+        System.out.println(methods.evilE("FLEX"));
+        System.out.println(methods.evilE("bean"));
+        System.out.println(methods.evilE("aaxEXbb"));
+        System.out.println(methods.evilE("EEE"));
+        System.out.println(methods.evilE("EXE"));
 
-//        String[] students = {"Alice", "Bob", "Carol", "Doug", "Ernie", "Fawn", "George", "Harry", "Isabella",
-//"Jessica"};
-//        System.out.println(Arrays.toString(methods.formClub(students, 2)));
-//        System.out.println(Arrays.toString(methods.formClub(students, 8)));
-//        System.out.println(Arrays.toString(methods.formClub(students, 0)));
+        String[] students = {"Alice", "Bob", "Carol", "Doug", "Ernie", "Fawn", "George", "Harry", "Isabella",
+"Jessica"};
+        System.out.println(Arrays.toString(methods.formClub(students, 2)));
+        System.out.println(Arrays.toString(methods.formClub(students, 8)));
+        System.out.println(Arrays.toString(methods.formClub(students, 0)));
 
-//        System.out.println(methods.countMultiples(25));
-//        System.out.println(methods.countMultiples(11));
-//        System.out.println(methods.countMultiples(49));
+        System.out.println(methods.countMultiples(25));
+        System.out.println(methods.countMultiples(11));
+        System.out.println(methods.countMultiples(49));
 
-//        System.out.println(methods.pickyCaptain(5,3,6,10));
-//        System.out.println(methods.pickyCaptain(6,3,5,9));
-//        System.out.println(methods.pickyCaptain(6,0,6,2));
-//        System.out.println(methods.pickyCaptain(6,7,5,5));
+        System.out.println(methods.pickyCaptain(5,3,6,10));
+        System.out.println(methods.pickyCaptain(6,3,5,9));
+        System.out.println(methods.pickyCaptain(6,0,6,2));
+        System.out.println(methods.pickyCaptain(6,7,5,5));
 
-//        System.out.println(methods.isSum(2, 5, 8));
-//        System.out.println(methods.isSum(5, 8, 3));
-//        System.out.println(methods.isSum(8, 4, 4));
+        System.out.println(methods.isSum(2, 5, 8));
+        System.out.println(methods.isSum(5, 8, 3));
+        System.out.println(methods.isSum(8, 4, 4));
 
-//        System.out.println(methods.connectStrings("hello", "back"));
-//        System.out.println(methods.connectStrings("hello", "bad"));
-//        System.out.println(methods.connectStrings("you", "good"));
-//        System.out.println(methods.connectStrings("good", "game"));
-//        System.out.println(methods.connectStrings("abc", "four"));
+        System.out.println(methods.connectStrings("hello", "back"));
+        System.out.println(methods.connectStrings("hello", "bad"));
+        System.out.println(methods.connectStrings("you", "good"));
+        System.out.println(methods.connectStrings("good", "game"));
+        System.out.println(methods.connectStrings("abc", "four"));
 
-//        String[] myArray = {"a","b","c","d","c","b","b"};
-//        System.out.println(methods.formReversedString(myArray, 1));
-//        System.out.println(methods.formReversedString(myArray, 0));
-//        System.out.println(methods.formReversedString(myArray, 12));
+        String[] myArray = {"a","b","c","d","c","b","b"};
+        System.out.println(methods.formReversedString(myArray, 1));
+        System.out.println(methods.formReversedString(myArray, 0));
+        System.out.println(methods.formReversedString(myArray, 12));
 
-//        System.out.println(methods.happyMonkey(11, true));
-//        System.out.println(methods.happyMonkey(4, false));
-//        System.out.println(methods.happyMonkey(4, true));
+        System.out.println(methods.happyMonkey(11, true));
+        System.out.println(methods.happyMonkey(4, false));
+        System.out.println(methods.happyMonkey(4, true));
 
 
-//        System.out.println(methods.medianOf3(1, 2, 3));
-//        System.out.println(methods.medianOf3(3, 1, 2));
-//        System.out.println(methods.medianOf3(3, 2, 1));
-//        System.out.println(methods.medianOf3(3, 1, 1));
+        System.out.println(methods.medianOf3(1, 2, 3));
+        System.out.println(methods.medianOf3(3, 1, 2));
+        System.out.println(methods.medianOf3(3, 2, 1));
+        System.out.println(methods.medianOf3(3, 1, 1));
     }
 }
