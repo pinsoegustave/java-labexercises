e219381ab2aa54b12b6e1d73f3466a37aa958711
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-12-14 20:42:43 -0500

    Final Revision: Test BODMAS on operations

--- FinalExamRevision/src/Main.java
+++ FinalExamRevision/src/Main.java
@@ -2,14 +2,8 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test first two lines");
 
-        String name = "Gustave";
-        char letter = 'd';
-
-        BigInteger num = new BigInteger("22008947623");
-
-        System.out.println(num);
-
+        int m = 2 + 5 * 4 - (3 + 2);
+        System.out.println(m);
     }
 }
