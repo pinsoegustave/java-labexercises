f084c9db9998e3642b27ff9c6c2e27616c188c3f
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-12-14 21:43:06 -0500

    Final Revision: Output operations

---
+++ FinalExamRevision/src/Lesson2.java
@@ -0,0 +1,18 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        int x, y;
+        String z = "3";
+        x = 6;
+        y = 4;
+
+        System.out.println(x);
+        System.out.println("x"); // Prints x as a message
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(x + y);   // Adds x and y, returns answer
+        System.out.println(y + z);   // Joins y and z, z is a string so, answer = 43
+        System.out.println(y + "x");  // Concatenates int with string, answer = 4x
+
+
+    }
+}
