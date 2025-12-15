2252cf2444f145b0f8ab213b0eae048a5d0af219
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-12-14 22:53:11 -0500

    Final Revision: Conditionals

---
+++ FinalExamRevision/src/Lesson3.java
@@ -0,0 +1,8 @@
+public class Lesson3 {
+    public static void main(String[] args) {
+
+        int a = 3;
+        if (a > 5) System.out.println("Greater");
+        else System.out.println("lower");
+    }
+}
