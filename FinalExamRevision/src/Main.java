b5254fcf6a58e9b051f1f85e05ff76a4aa410da2
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-12-14 20:37:03 -0500

    Final Revision: variable declaration

--- FinalExamRevision/src/Main.java
+++ FinalExamRevision/src/Main.java
@@ -1,5 +1,15 @@
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test first two lines");
+
+        String name = "Gustave";
+        char letter = 'd';
+
+        BigInteger num = new BigInteger("22008947623");
+
+        System.out.println(num);
+
     }
 }
