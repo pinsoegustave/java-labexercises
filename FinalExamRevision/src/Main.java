a677bad03e6bb77b45a443351ce6aa836b1e5258
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-12-14 21:16:53 -0500

    Final Revision: Revision on Errors

--- FinalExamRevision/src/Main.java
+++ FinalExamRevision/src/Main.java
@@ -3,15 +3,20 @@ import java.math.BigInteger;
 public class Main {
     public static void main(String[] args) {
 
-        int x = 5;
-        int y = 3;
+//        int minute = 299;
+//        double answer = (double) minute / 60;
+//        System.out.println(answer);
 
-        x = x + 4;
-        x+=4;
-        x-=2;
-        x*=4;
-        x/=11;
+        int a = 12;
+//        int b = 6   This is compiled (syntax) error
+        int b = 6;
+        int c = 0;
+         int d = a - b;
+        double e = a / c;
 
-        System.out.println(x);
+//        System.out.println("a + b = "+ d); (Logic error) According to the message, the expected answer is not what u will get.
+        System.out.println("a + b = "+ d);
+//        System.out.println("a / c = "+ e ); (Runtime error) Dividing by 0 is not a correct math operation
+        System.out.println("a / c = "+ e );
     }
 }
