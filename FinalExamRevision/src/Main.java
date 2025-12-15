bc04b4c58937863c782471a5d437c86d5df23341
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-12-14 20:49:50 -0500

    Final Revision: Simple operations

--- FinalExamRevision/src/Main.java
+++ FinalExamRevision/src/Main.java
@@ -5,5 +5,16 @@ public class Main {
 
         int m = 2 + 5 * 4 - (3 + 2);
         System.out.println(m);
+
+        int x = 5;
+        int y = 3;
+
+//        x + 1 = 6;   This is an error because there is no variable declared
+//        x = 17 + 1;  // This overrides the value of x and changes to the answer of 17 + 1;
+//        x = y;  This changes the value of x to the value of y
+//        x = y + 1; This changes the value of x to the answer of y + 1
+        x = x + 1;   // Adds 1 to the value of x
+
+        System.out.println(x);
     }
 }
